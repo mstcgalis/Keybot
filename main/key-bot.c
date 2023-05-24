7ddcf4f3593d3a9e4fbbf9941777121ed2732178
Daniel Gális <danielgalis21@gmail.com>
2023-05-24 20:09:25 +0200

    switched to idf v4.4.4

--- main/key-bot.c
+++ main/key-bot.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
+
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -17,6 +18,8 @@
 #include "driver/gpio.h"
 #include "driver/touch_pad.h"
 #include "led_strip.h"
+#include "sdkconfig.h"
+#include "driver/rmt.h"
 
 static const char *TAG = "key-bot";
 
@@ -28,15 +31,12 @@ static const char *TAG = "key-bot";
 // Set the time threshold in seconds (how long has the change in touch_value be to change the key_state bool)
 #define TIME_THRESHOLD 1
 
-//// WIFI
-#ifndef INET6_ADDRSTRLEN
-#define INET6_ADDRSTRLEN 48
-#endif
+// //// WIFI
 /*set the ssid and password via "idf.py menuconfig"*/
 #define DEFAULT_SSID CONFIG_EXAMPLE_WIFI_SSID
 #define DEFAULT_PWD CONFIG_EXAMPLE_WIFI_PASSWORD
+
 #define DEFAULT_LISTEN_INTERVAL CONFIG_EXAMPLE_WIFI_LISTEN_INTERVAL
-#define DEFAULT_BEACON_TIMEOUT  CONFIG_EXAMPLE_WIFI_BEACON_TIMEOUT
 
 #if CONFIG_EXAMPLE_POWER_SAVE_MIN_MODEM
 #define DEFAULT_PS_MODE WIFI_PS_MIN_MODEM
@@ -45,14 +45,17 @@ static const char *TAG = "key-bot";
 #elif CONFIG_EXAMPLE_POWER_SAVE_NONE
 #define DEFAULT_PS_MODE WIFI_PS_NONE
 #else
-
 #define DEFAULT_PS_MODE WIFI_PS_NONE
 #endif /*CONFIG_POWER_SAVE_MODEM*/
 
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
+
 static void wifi_power_save(void);
 
-//// TIME
+// //// TIME
 static void obtain_time(void);
+
+static void initialize_sntp(void);
 
 void time_sync_notification_cb(struct timeval *tv)
 {
@@ -66,15 +69,17 @@ void time_sync_notification_cb(struct timeval *tv)
 #define LED_STRIP_LED_NUMBERS 1
 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
 #define LED_STRIP_RMT_RES_HZ  (10 * 1000 * 1000)
+// RMT channel for LED strip
+#define RMT_TX_CHANNEL RMT_CHANNEL_0
 
 // Time duration of the visibility LED
 #define VISIBILITY_LED_DURATION_S 15
 // Visibility LED state bool
 bool visibility_led_state = false;
 
-static void blink_led(led_strip_handle_t led_strip, uint8_t red, uint8_t green, uint8_t blue);
+static void blink_led(led_strip_t *strip, uint8_t red, uint8_t green, uint8_t blue);
 
-static bool visibility_led(led_strip_handle_t led_strip, bool visibility_led);
+static bool visibility_led(led_strip_t *strip, bool visibility_led);
 
 //// MAIN
 void app_main(void)
@@ -100,35 +105,29 @@ void app_main(void)
     touch_pad_init();
     touch_pad_config(TOUCH_PAD_NUM, 0);
     // Set up GPIO for the capacitive sensor
-    esp_rom_gpio_pad_select_gpio(TOUCH_PAD_NUM);
+    gpio_pad_select_gpio(TOUCH_PAD_NUM);
     gpio_set_direction(TOUCH_PAD_NUM, GPIO_MODE_INPUT);
 
     //// LED SETUP
-    // LED Strip object handle
-    led_strip_handle_t led_strip;
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(LED_STRIP_BLINK_GPIO, RMT_TX_CHANNEL);
+    // set counter clock to 40MHz
+    config.clk_div = 2;
 
-    // LED strip general initialization, according to your led board design
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = LED_STRIP_BLINK_GPIO,   // The GPIO that connected to the LED strip's data line
-        .max_leds = LED_STRIP_LED_NUMBERS,        // The number of LEDs in the strip,
-        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
-        .led_model = LED_MODEL_WS2812,            // LED strip model
-        .flags.invert_out = false,                // whether to invert the output signal
-    };
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 
-    // LED strip backend configuration: RMT
-    led_strip_rmt_config_t rmt_config = {
-        .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
-        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
-        .flags.with_dma = false,               // DMA feature is available on ESP target like ESP32-S3
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    // install ws2812 driver
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(LED_STRIP_LED_NUMBERS, (led_strip_dev_t)config.channel);
+    led_strip_t *strip = led_strip_new_rmt_ws2812(&strip_config);
+    if (!strip) {
+        ESP_LOGE(TAG, "install WS2812 driver failed");
+    }
     ESP_LOGI(TAG, "Created LED strip object with RMT backend");
 
     //// VARIABLES
     // key is logged as present on the sensor nail
     bool key_state = false;
-    // OEMENTARY key state key is present on the nail (right now)
+    // MOEMENTARY key state key is present on the nail (right now)
     bool momentary_key_state = false;
     // time the sensor spent below the threshold in milliseconds (momentary_key_state = true)
     uint32_t time_below_threshold_ms = 0;
@@ -136,6 +135,7 @@ void app_main(void)
     uint32_t time_above_threshold_ms = 0;
     // current touch value
     uint16_t touch_value;
+
     // time the visibility LED was turned on
     time_t visibility_led_on_time = 0; 
    
@@ -169,23 +169,30 @@ void app_main(void)
                     // Notify users of key state change
                     if (key_state) {
                         // Turn off visibility LED
-                        visibility_led_state = visibility_led(led_strip, false);
+                        visibility_led_state = visibility_led(strip, false);
+                        // wait for 0.1 second
+                        vTaskDelay(pdMS_TO_TICKS(100));
                         // Green LED feedback
-                        blink_led(led_strip, 0, 255, 0);
+                        blink_led(strip, 0, 255, 0);
                         // TODO: Send notification to Discord
                     } else {
                         // Turn off visibility LED
-                        visibility_led_state = visibility_led(led_strip, false);
+                        visibility_led_state = visibility_led(strip, false);
+                        // wait for 0.1 second
+                        vTaskDelay(pdMS_TO_TICKS(100));
                         // Red LED feedback
-                        blink_led(led_strip, 255, 0, 0);
+                        blink_led(strip, 255, 0, 0);
+                        // wait for 0.2 second
+                        vTaskDelay(pdMS_TO_TICKS(200));
                         // Turn on visibility LED
-                        visibility_led_state = visibility_led(led_strip, true);
+                        visibility_led_state = visibility_led(strip, true);
                         // save time visibility LED was turned on
                         visibility_led_on_time = now;
+                        // TODO: Send notification to Discord
                     }
 
                     // Log current time | key state
-                    ESP_LOGI(TAG, "%s | Key: %s", strftime_buf, key_state ? "present" : "not present");
+                    ESP_LOGI(TAG, "%s | Key: %s", strftime_buf,key_state ? "present" : "not present");
                     // Reset time_above_threshold_ms and time_below_threshold_ms
                     time_above_threshold_ms = 0;
                     time_below_threshold_ms = 0;
@@ -202,7 +209,7 @@ void app_main(void)
         // Check if visibility LED should be turned off
         if (visibility_led_state && (now - visibility_led_on_time) >= VISIBILITY_LED_DURATION_S) {
             // Turn off visibility LED
-            visibility_led_state = visibility_led(led_strip, false);
+            visibility_led_state = visibility_led(strip, false);
         }
    
         // Sleep for 100ms
@@ -210,7 +217,7 @@ void app_main(void)
         }
 }
 
-// TIME
+//// TIME
 static void obtain_time(void)
 {
     // Initialize NVS
@@ -225,7 +232,15 @@ static void obtain_time(void)
     // Configure dynamic frequency scaling:
     // maximum and minimum frequencies are set in sdkconfig,
     // automatic light sleep is enabled if tickless idle support is enabled.
-    esp_pm_config_t pm_config = {
+#if CONFIG_IDF_TARGET_ESP32
+    esp_pm_config_esp32_t pm_config = {
+#elif CONFIG_IDF_TARGET_ESP32S2
+    esp_pm_config_esp32s2_t pm_config = {
+#elif CONFIG_IDF_TARGET_ESP32C3
+    esp_pm_config_esp32c3_t pm_config = {
+#elif CONFIG_IDF_TARGET_ESP32S3
+    esp_pm_config_esp32s3_t pm_config = {
+#endif
             .max_freq_mhz = CONFIG_EXAMPLE_MAX_CPU_FREQ_MHZ,
             .min_freq_mhz = CONFIG_EXAMPLE_MIN_CPU_FREQ_MHZ,
 #if CONFIG_FREERTOS_USE_TICKLESS_IDLE
@@ -235,29 +250,41 @@ static void obtain_time(void)
     ESP_ERROR_CHECK( esp_pm_configure(&pm_config) );
 #endif // CONFIG_PM_ENABLE
 
+    /*establish wifi connection*/
     wifi_power_save();
 
-    ESP_LOGI(TAG, "Initializing and starting SNTP");
-    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_SNTP_TIME_SERVER);
-    config.sync_cb = time_sync_notification_cb;
+    initialize_sntp();
 
-    esp_netif_sntp_init(&config);
+    ESP_LOGI(TAG, "Initializing and starting SNTP");
 
     // wait for time to be set
-    time_t now = 1000000000000;
+    time_t now = 0;
     struct tm timeinfo = { 0 };
     int retry = 0;
-    const int retry_count = 15;
-    while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && ++retry < retry_count) {
+    const int retry_count = 10;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
     time(&now);
     localtime_r(&now, &timeinfo);
-
-    esp_netif_sntp_deinit();
 }
 
-// EVENT HANDLER (wifi)
+//// SNTP
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+#endif
+    sntp_init();
+}
+
+//// WIFI
+/*event handler for wifi*/
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
@@ -295,42 +322,51 @@ static void wifi_power_save(void)
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_set_inactive_time(WIFI_IF_STA, DEFAULT_BEACON_TIMEOUT));
 
     ESP_LOGI(TAG, "esp_wifi_set_ps().");
     esp_wifi_set_ps(DEFAULT_PS_MODE);
 }
 
-// LED
+//// LED
 // Blink LED strip 2 times at specified RGB color, used for key state change and knock command
-static void blink_led(led_strip_handle_t led_strip, uint8_t red, uint8_t green, uint8_t blue) {
+static void blink_led(led_strip_t *strip, uint8_t red, uint8_t green, uint8_t blue) {
     ESP_LOGI(TAG, "Blinking LED 2 times, color: %u %u %u", red, green, blue);
     for (int i = 0; i < 2; i++) {
+        // Clear all pixels
+        ESP_ERROR_CHECK(strip->clear(strip, 50));
         // Set the LED pixel using the specified RGB values
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, green, red, blue));
-        // Refresh the strip to send data
-        ESP_ERROR_CHECK(led_strip_refresh(led_strip));
-        // Wait for 50ms
-        vTaskDelay(pdMS_TO_TICKS(50));
-        /* Set all LED off to clear all pixels */
-        ESP_ERROR_CHECK(led_strip_clear(led_strip));
-        // Wait for 500ms
-        vTaskDelay(pdMS_TO_TICKS(150));
+        ESP_ERROR_CHECK(strip->set_pixel(strip, 0, green, red, blue));
+        // Flush RGB values to LEDs
+        ESP_ERROR_CHECK(strip->refresh(strip, 50));
+        // Wait for 100ms
+        vTaskDelay(pdMS_TO_TICKS(100));
+        // Clear all pixels
+        ESP_ERROR_CHECK(strip->clear(strip, 50));
+        // Wait for 100ms
+        vTaskDelay(pdMS_TO_TICKS(100));
     };
 }
 
 // Toggle visibility (white) LED
-static bool visibility_led(led_strip_handle_t led_strip, bool visibility_led) {
+static bool visibility_led(led_strip_t *strip, bool visibility_led) {
     if (visibility_led) {
-        ESP_LOGI(TAG, "Visibility LED turned ON");
-        // Set the LED pixel using the specified RGB values
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, 255, 255, 255));
-        // Refresh the strip to send data
-        ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+        ESP_LOGI(TAG, "Visibility LED turning ON");
+        // Clear all pixels
+        ESP_ERROR_CHECK(strip->clear(strip, 50));
+        // // Gradually increase brightness and whiteness over 10 seconds
+        // for (int i = 0; i <= 255; i++) {
+        //     ESP_ERROR_CHECK(strip->set_pixel(strip, 0, i, i, i));
+        //     ESP_ERROR_CHECK(strip->refresh(strip, 39)); // 10000ms / 255 steps = 39.22ms per step
+        // }
+        // Set the LED pixel using the white color
+        ESP_ERROR_CHECK(strip->set_pixel(strip, 0, 255, 255, 255));
+        // Flush RGB values to LEDs
+        ESP_ERROR_CHECK(strip->refresh(strip, 50));
     } else {
-        ESP_LOGI(TAG, "Visibility LED turned OFF");
-        /* Set all LED off to clear all pixels */
-        ESP_ERROR_CHECK(led_strip_clear(led_strip));
+        ESP_LOGI(TAG, "Visibility LED turning OFF");
+        // Clear all pixels
+        ESP_ERROR_CHECK(strip->clear(strip, 50));
     };
     return visibility_led;
 }
+
